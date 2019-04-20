f50edfdbd6b36b24327b9107205131e5715ee961
marcosaugustomodesto <marcosaugustomodesto15@gmail.com>
2019-04-20 18:51:12 -0300

    all

---
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,26 @@
+class ListaNegociacoes {
+    constructor(armadilha) {
+    //constructor(contexto, armadilha) {
+        this._armadilha = armadilha;
+		//this._contexto = contexto;
+        this._negociacoes = [];
+    }
+    
+    adiciona(negociacao) {
+        
+        this._negociacoes.push(negociacao);
+		this._armadilha(this);
+		//Reflect.apply(this._armadilha, this._contexto, [this]);
+    }
+    
+    get negociacoes() {        
+        return [].concat(this._negociacoes);
+		
+    }
+	
+	esvazia(){
+		this._negociacoes = [];
+		this._armadilha(this);
+		//Reflect.apply(this._armadilha, this._contexto, [this]);
+	}
+}
